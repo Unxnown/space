3b9d378fd8bf5c3dd73dcb5ba8ea21e3c76c8098
Carina Limon <limoncarina@gmail.com>
2017-05-01 21:03:50 -0700

    first commit. added player, player controller, camera, lighting, and background.

---
+++ space/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary {
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public Boundary boundary;
+	public float tilt;
+
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		Rigidbody rigidbody = GetComponent<Rigidbody> ();
+		rigidbody.velocity = movement * speed;
+
+		float newX = Mathf.Clamp (rigidbody.position.x, boundary.xMin, boundary.xMax);
+		float newZ = Mathf.Clamp (rigidbody.position.z, boundary.zMin, boundary.zMax);
+
+		rigidbody.position = new Vector3 (newX, 0.0f, newZ);
+
+		rigidbody.rotation = Quaternion.Euler (0.0f, 0.0f, rigidbody.velocity.x * -tilt);
+	}
+}
